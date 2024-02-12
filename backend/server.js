c4ef5c5bc94b9583538566bd08a4bd1b69adb974
mattcmerritt <mattcmerritt@gmail.com>
2024-02-11 23:03:49 -0500

    allowed communication between server and client

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,16 @@ const ws = require('ws')
 
 const wss = new ws.WebSocketServer({ port: 8080 })
 
+console.log('Started server on port 8080')
+
 wss.on('connection', function connection(socket) {
     console.log('Client connected!')
+
+    socket.on('message', (data) => {
+        console.log(`Message received from client: "${data}"`)
+
+        socket.send(`Server received: ${data}`)
+    })
 
     socket.on('close', () => {
         console.log('Client disconnected!')
