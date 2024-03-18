5894240ac4ab3ce2520000e87a6a8a1b5af3272c
mikepmerritt <mikepmerritt@gmail.com>
2024-03-18 03:44:50 -0400

    basic element layout of cards and columns

--- frontend/app/KanbanCard.tsx
+++ frontend/app/KanbanCard.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 export default function KanbanCard(props : { value : any, onChange : any }) {
+  function openChat() {
+    console.log("the chat has been opened")
+  }
+  
   return (
-    <div className="py-1 bg-blue-400" id="kanbancard">
-      <label htmlFor="name-input">This is a card</label>
-      <textarea className="" id="name-input" onChange={props.onChange} />
-      <br />
+    <div className="m-2 p-1 flex flex-col bg-blue-300 ring-2 ring-blue-500 rounded-lg" id="kanbancard">
+      <input className="m-2 bg-blue-200" id="name-input" type="text" onChange={props.onChange} />
+      <button className="m-1 ring-2 ring-gray-950" onClick={openChat}>View Chat</button>
     </div>
   )
 }
--- frontend/app/KanbanColumn.tsx
+++ frontend/app/KanbanColumn.tsx
@@ -1,16 +1,42 @@
 'use client'
 
 import { useState } from 'react'
+import KanbanCard from "./KanbanCard.tsx"
 
 export default function KanbanColumn(props : { value : any, onChange : any }) {
+  type Card = {
+    id : string,
+    name : string
+    // TODO: maybe add more here
+  }
+  
+  const [cards, setCards] = useState([] as Card[])
+  
   function addCard() {
-    console.log("added a card")
+    setCards(c => c.concat({id:"newcard", name:"newname"}))
+  }
+
+  function generateCardsForColumn(value : any) {
+    const cardComponents : JSX.Element[] = []
+
+    if (cards) {
+      cards?.forEach(card => {
+        cardComponents.push(
+          <KanbanCard 
+            value={card.name}
+            onChange={() => console.log("card change?")}
+          />
+        )
+      })
+    }
+
+    return cardComponents
   }
 
   return (
-    <div className="m-2 flex flex-col py-1 bg-blue-400" id="kanban-column">
+    <div className="m-2 flex flex-col bg-blue-400" id="kanban-column">
       <input className="m-1 ring-1 ring-gray-950" id="column-title" type="text" onChange={props.onChange} />
-      <br />
+      {generateCardsForColumn(props.value)}
       <button className="m-1 ring-2 ring-gray-950" onClick={addCard}>Add Card</button>
     </div>
   )
