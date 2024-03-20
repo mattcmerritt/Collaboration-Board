82cc48d3cda54ac7c4fccbdc85350c4cfda65634
mikepmerritt <mikepmerritt@gmail.com>
2024-03-20 18:12:59 -0400

    cards are now draggable

--- frontend/app/KanbanCard.tsx
+++ frontend/app/KanbanCard.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import Draggable from 'react-draggable';
+
 export default function KanbanCard(props : { id : any, name : string, col : any, ws : WebSocket, colCount : any, conversationRef : any }) {
   function openChat() {
     props.conversationRef.current = `${props.id}`
@@ -47,13 +49,16 @@ export default function KanbanCard(props : { id : any, name : string, col : any,
   }
   
   return (
-    <div className="m-2 p-1 flex flex-col bg-blue-300 ring-2 ring-blue-500 rounded-lg" id={"kanban-card-" + props.id}>
-      <textarea className="m-2 px-1 bg-blue-200 rounded-lg" id={"card-name-" + props.id} onChange={updateCardText} value={props.name}/>
-      <button className="m-1 ring-2 ring-gray-950" onClick={openChat}>View Chat</button>
-      <div className="flex flex-row">
-        <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(-1)}>Move left</button>
-        <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(1)}>Move right</button>
+    <Draggable>
+      <div className="m-2 p-1 flex flex-col bg-blue-300 ring-2 ring-blue-500 rounded-lg" id={"kanban-card-" + props.id}>
+        <textarea className="m-2 px-1 bg-blue-200 rounded-lg" id={"card-name-" + props.id} onChange={updateCardText} value={props.name}/>
+        <button className="m-1 ring-2 ring-gray-950" onClick={openChat}>View Chat</button>
+        <div className="flex flex-row">
+          <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(-1)}>Move left</button>
+          <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(1)}>Move right</button>
+        </div>
       </div>
-    </div>
+    </Draggable>
+    
   )
 }
