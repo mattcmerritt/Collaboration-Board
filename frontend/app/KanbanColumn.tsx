c62d03a4b2f7e60ff27c58015a4c8528dee20478
mikepmerritt <mikepmerritt@gmail.com>
2024-04-01 07:01:38 -0400

    hover events for columns

--- frontend/app/KanbanColumn.tsx
+++ frontend/app/KanbanColumn.tsx
@@ -108,8 +108,14 @@ export default function KanbanColumn(props : { colNum : any, cardCount : Mutable
     }))
   }
 
+  // hover functionality
+  function handleColumnHovered() {
+    console.log("hovering col " + props.colNum)
+    // likely set some state var here, that can be called in page to move around cards or in KanbanCard on release
+  }
+
   return (
-    <div className="m-2 flex flex-col bg-blue-400" id={"kanban-column-" + props.colNum}>
+    <div className="m-2 flex flex-col bg-blue-400" id={"kanban-column-" + props.colNum} onMouseEnter={handleColumnHovered}>
       <input className="m-1 px-1 bg-blue-300 ring-2 ring-blue-500 rounded-lg" id={"column-title-" + props.colNum} type="text" onChange={updateColumnName} />
       {generateCardsForColumn(props.colNum)}
       <button className="m-1 ring-2 ring-gray-950" onClick={addCard}>Add Card</button>
