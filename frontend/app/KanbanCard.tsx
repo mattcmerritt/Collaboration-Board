9e10637f3a49ffff02d395df0bf417ee0c751725
mikepmerritt <mikepmerritt@gmail.com>
2024-04-15 09:21:02 -0400

    fixed bug that caused cards to disappear if dropped in same column

--- frontend/app/KanbanCard.tsx
+++ frontend/app/KanbanCard.tsx
@@ -32,32 +32,25 @@ export default function KanbanCard(props : { id : any, name : string, col : any,
     props.setActiveCardName(nameInput.value)
   }
 
-  function moveCard(change : number) {
-    // check to see if move is valid first
-    if((props.col + change) > 0 && (props.col + change) < props.colCount)
-    {
-      // fetch name of card
-      const nameInput : HTMLInputElement | null = document.getElementById("card-name-" + props.id) as HTMLInputElement
-    
-      // send request to put in the new card
-      props.ws.send(JSON.stringify({
-        "ws_msg_type": "move card",
-        "id": props.id,
-        "name": nameInput.value,
-        "column": props.col + change
-      }))
+  function moveCardRequest(newCol : number) {
+    // fetch name of card
+    const nameInput : HTMLInputElement | null = document.getElementById("card-name-" + props.id) as HTMLInputElement
+  
+    // send request to put in the new card
+    props.ws.send(JSON.stringify({
+      "ws_msg_type": "move card",
+      "id": props.id,
+      "name": nameInput.value,
+      "column": newCol
+    }))
 
-      // send request to remove the old card
-      props.ws.send(JSON.stringify({
-        "ws_msg_type": "remove card",
-        "id": props.id,
-        "name": nameInput.value.trim(),
-        "column": props.col
-      }))
-    }
-    else {
-      console.log("bounds exception on moving card " + props.id + " from " + props.col + " to " + (props.col + change))
-    }
+    // send request to remove the old card
+    props.ws.send(JSON.stringify({
+      "ws_msg_type": "remove card",
+      "id": props.id,
+      "name": nameInput.value,
+      "column": props.col
+    }))
   }
 
   function updateCardText() {
@@ -93,7 +86,11 @@ export default function KanbanCard(props : { id : any, name : string, col : any,
 
     // place card into column
     if(destinationCol !== null && replacementCard !== null) {
-      destinationCol?.appendChild(replacementCard)
+      if(hoveredCol !== props.col)
+      {
+        moveCardRequest(hoveredCol)
+      }
+      // destinationCol?.appendChild(replacementCard)
     }
   }
 
@@ -102,8 +99,6 @@ export default function KanbanCard(props : { id : any, name : string, col : any,
       <textarea className="m-2 px-1 bg-blue-200 rounded-lg" id={"card-name-" + props.id} onChange={updateCardText} value={props.name}/>
       <button className="m-1 ring-2 ring-gray-950" onClick={openChat}>View Chat</button>
       <div className="flex flex-row">
-        <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(-1)}>Move left</button>
-        <button className="m-1 flex-1 bg-blue-300 ring-2 ring-gray-950 rounded-lg" onClick={() => moveCard(1)}>Move right</button>
       </div>
     </div>
   )
